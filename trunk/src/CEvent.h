526285e920825e25472a5169a20eb3d53c658874
lachupe <lachupe@4015f8a6-a02a-0410-a06c-ff4bd89d836d>
2007-05-24 22:05:12 +0000

    yes, adde the CEvent.c file

---
+++ trunk/src/CEvent.h
@@ -0,0 +1,78 @@
+#ifndef CEVENT_H
+#define CEVENT_H
+
+#include <QQueue>
+#include <QMutex>
+
+class Event   {
+    public:
+        Event() {}
+        Event(const Event &other) 
+        {
+            dir = other.dir;
+            name = other.name;
+            desc = other.desc;
+            exits = other.exits;
+            blind = other.blind;
+            terrain = other.terrain;
+            prompt = other.prompt;
+            movement = other.movement;
+        }
+
+        Event &operator=(const Event &other) 
+        {
+            if (this != &other) {  // make sure not same object
+                dir = other.dir;
+                name = other.name;
+                desc = other.desc;
+                exits = other.exits;
+                blind = other.blind;
+                terrain = other.terrain;
+                prompt = other.prompt;
+                movement = other.movement;
+            }
+            return *this;    // Return ref for multiple assignment            
+        }
+
+        void clear()    {
+            dir = "";
+            name = "";
+            desc = "";
+            exits = "";
+            prompt = "";
+            blind = false;
+            movement = false;
+            terrain = -1;
+        }
+
+        QByteArray dir;
+        QByteArray name;
+        QByteArray desc;
+        QByteArray exits;
+        bool       blind;         /* fog, no light, blind flag */
+        bool       movement;    
+        char       terrain;
+        QByteArray prompt;
+};
+
+class PipeManager {
+    QMutex pipeMutex;
+    QQueue<Event> Pipe;
+
+public:
+    void addEvent(Event e)  { pipeMutex.lock(); Pipe.enqueue(e); pipeMutex.unlock();  }
+    void clear() { pipeMutex.lock(); Pipe.clear(); pipeMutex.unlock();  }
+    bool isEmpty() { return Pipe.empty(); }    
+
+    Event getEvent() 
+    {
+        Event event;
+        pipeMutex.lock(); 
+        event = Pipe.dequeue(); 
+        pipeMutex.unlock(); 
+        return event;
+    }; 
+};
+
+
+#endif
