e941ba9ad657e76c4ea66b2d046d39520de4a583
lachupe <lachupe@gmail.com>
2008-01-15 11:56:04 +0000

    fixing 2

--- src/xml2.h
+++ src/xml2.h
@@ -1,4 +1,5 @@
-
+#ifndef XML2_H                                                                                                                                              
+#define XML2_H  
 /* buffered reading from file */
 
 
@@ -7,3 +8,4 @@ void xml_writebase(QString filename);
 /* Loads a char, returns char struct or NULL */
 void xml_readbase(QString filename);
 
+#endif
